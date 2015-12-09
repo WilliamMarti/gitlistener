229bfe31745d90c4e8b7f32508c85f889d09b704
Ben Marti <kareas1115@gmail.com>
2015-12-09 15:11:36 -0800

    added json parseer and key verification

--- gitlistener.py
+++ gitlistener.py
@@ -1,21 +1,38 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 from subprocess import call
-import git
+import git, json
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['POST'])
 def index():
 	
-	filelocation = "/home/wmarti/github/lunch_app"
+	jsondata = request.get_json()
 
-	g = git.cmd.Git(filelocation)
-	g.pull()
-	return "Hello" 
+	
+	passedpw =  jsondata["hook"]["config"]["secret"]
+
+	f = open('keyfile.txt', 'r')
+	key = f.readline().strip()
+
+	if (passedpw == key):
+		print "Correct"
+
+		filelocation = "/home/wmarti/github/lunch_app"
+
+		g = git.cmd.Git(filelocation)
+		g.pull()
+
+	else:
+		
+		print "Wrong"
+
+	return "Ran"
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5001)
+    app.run(debug=True,host='0.0.0.0',port=5001)
 
 
 
