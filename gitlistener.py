bad1ab0cda43e3cf255754f8df01cb85a60e128c
Ben Marti <kareas1115@gmail.com>
2015-12-09 19:44:04 -0500

    changed to anything being posted

--- gitlistener.py
+++ gitlistener.py
@@ -1,38 +1,33 @@
 from flask import Flask
 from flask import render_template, request
 from subprocess import call
-import git, json
+import git, json, os
+
+newname = "gitlistener"
+from ctypes import cdll, byref, create_string_buffer
+    
+libc = cdll.LoadLibrary('libc.so.6')    #Loading a 3rd party library C
+buff = create_string_buffer(len(newname)+1) #Note: One larger than the name (man prctl says that)
+buff.value = newname                 #Null terminated string as it should be
+libc.prctl(15, byref(buff), 0, 0, 0) #Refer to "#define" of "/usr/include/linux/prctl.h" for the misterious value 16 & arg[3..5] are zero as the man page says.
+
 
 app = Flask(__name__)
 
 @app.route("/", methods=['POST'])
 def index():
-	
-	jsondata = request.get_json()
 
-	
-	passedpw =  jsondata["hook"]["config"]["secret"]
+	if request.method == 'POST':
 
-	f = open('keyfile.txt', 'r')
-	key = f.readline().strip()
-
-	if (passedpw == key):
-		print "Correct"
-
-		filelocation = "/home/wmarti/github/lunch_app"
-
-		g = git.cmd.Git(filelocation)
-		g.pull()
-
-	else:
-		
-		print "Wrong"
+		repo = git.Repo('/var/www/lunch_app')
+		print repo.git.status()
+		print repo.git.pull()
 
 	return "Ran"
 
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0',port=5001)
+    app.run(host='0.0.0.0',port=5001)
 
 
 
